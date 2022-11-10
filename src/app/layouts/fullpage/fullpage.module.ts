12d655386d0c7a43450c2351985e9b58805385a0
agorn <agorny112@gmail.com>
2022-11-10 02:20:30 +0100

    fullpage layout

---
+++ src/app/layouts/fullpage/fullpage.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FullpageComponent} from "./fullpage.component";
+import {RouterModule} from "@angular/router";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {SharedModule} from "../../shared/shared.module";
+import {LoginComponent} from "../../modules/login/login.component";
+
+@NgModule({
+  declarations: [
+    FullpageComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    SharedModule
+  ]
+})
+export class FullpageModule {
+}
